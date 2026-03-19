339c6733951858f20e2ffd6c3ebeb2de9509139e
HenriqueT28 <rickytavarescontact@gmail.com>
2026-03-18 22:27:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Biblia from "./Biblia";
 import Devocional from "./Devocional";
 import Estudo from "./Estudo";
 
+import ComoEstou from "./ComoEstou";
 const STYLE = `
 @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&display=swap');
 *{box-sizing:border-box;margin:0;padding:0}
